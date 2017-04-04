3f3ca94147c21072f712deaee6dfdf16f8ad7b95
Dablu <dablu.kumar@capitalfloat.com>
2017-04-04 22:07:42 +0530

    Added file for testing the basic logic of selenium

---
+++ src/main/java/com/capitalfloat/Main.java
@@ -0,0 +1,17 @@
+package com.capitalfloat;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Main {
+        public static void main(String[] args) {
+            // And now use this to visit Google
+            System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+            DesiredCapabilities cap = DesiredCapabilities.firefox();
+            cap.setCapability("marionette", true);
+            WebDriver driver = new FirefoxDriver(cap);
+            driver.get("http://www.google.com");
+
+        }
+}
